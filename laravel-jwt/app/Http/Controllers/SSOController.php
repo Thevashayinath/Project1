ca45c05d6fc161b38904196fcb53136754cadb88
Deva <deva.caralasingam@dishserve.com>
2023-05-10 01:08:07 +0530

    Improvement

--- laravel-jwt/app/Http/Controllers/SSOController.php
+++ laravel-jwt/app/Http/Controllers/SSOController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-
 
 class SSOController extends Controller
 {
@@ -13,7 +13,7 @@ class SSOController extends Controller
             return view('auth.login');
         }
 
-        $token = $request->header('Authorization');
+        $token = $request->bearerToken();
         $res = $this->validateToken($token);
         if (!$res) {
             return response()->json([
@@ -30,7 +30,7 @@ class SSOController extends Controller
         $response = Http::withHeaders([
             'Authorization' => 'Bearer ' . $token,
             'Accept' => 'application/json'
-        ])->post('http://127.0.0.1:8001/api/getToken');
+        ])->post('http://127.0.0.1:8001/api/checkToken');
 
         if ($response->ok()) {
             // API request successful, handle response
@@ -38,6 +38,22 @@ class SSOController extends Controller
         } else {
             // API request failed, handle error
             return $response->json()['error'];
+        }
+    }
+    public function checkToken(Request $request)
+    {
+        if ($request->header('Authorization')) {
+            $token = $request->bearerToken();
+            $user = User::where('remember_token', $token)->first();
+            if ($user){
+                return response()->json("success");
+            }
+            else{
+                return response()->json("error");
+            }
+        }
+        else{
+            return response()->json("error");
         }
     }
 }
--- laravel-jwt/routes/api.php
+++ laravel-jwt/routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SSOController;
-use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +17,5 @@ use App\Http\Controllers\AuthController;
 
 Route::controller(SSOController::class)->group(function () {
     Route::post('dashboard', 'dashboard');
-});
-
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
+    Route::post('checkToken', 'checkToken');
 });
